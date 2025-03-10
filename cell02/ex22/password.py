b04a7c4e042eb6b78995fc94db7d0f22f41425b6
Mitree2550 <Mitree2550@gmail.com>
2025-03-10 13:45:21 +0700

    password.py

---
+++ cell02/ex22/password.py
@@ -0,0 +1,8 @@
+secret_password = "new2550"
+
+entered_password = input("Enter password: ")
+
+if entered_password == secret_password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
